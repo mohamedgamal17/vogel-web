dfe54bcc77b0fa8c435ff67d03674806417432fd
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-04-22 07:41:29 +0200

    feat(auth): Add auth0 package and configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAuth0({
+      domain: environment.auth0.domain,
+      clientId: environment.auth0.clientId,
+      authorizationParams: {
+        redirect_uri: window.location.origin + '/',
+        audience: environment.auth0.audience,
+      },
+    })
   ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000',
+  auth0: {
+    domain: 'https://dev-86svyzlv1vxhmxlg.us.auth0.com',
+    clientId: 'r7d114nbfMWenIprAJnOlCvsDAJXeLmg',
+    audience: 'vogel-api',
+  },
 };
