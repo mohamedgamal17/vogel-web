68e1b90fed9cd55d318e851d843b9605547e4866
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-04-22 08:36:38 +0200

    refactor(landing): Auto redirect if user is logged in

--- src/app/features/landing/pages/landing-page/landing-page.ts
+++ src/app/features/landing/pages/landing-page/landing-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef } from '@angular/core';
 import { LandingFeatureHighlightsComponent } from '../../components/landing-feature-highlights/landing-feature-highlights';
 import { LandingFinalCtaComponent } from '../../components/landing-final-cta/landing-final-cta';
 import { LandingFooterComponent } from '../../components/landing-footer/landing-footer';
@@ -7,6 +7,9 @@ import { LandingHeroComponent } from '../../components/landing-hero/landing-hero
 import { LandingHowItWorksComponent } from '../../components/landing-how-it-works/landing-how-it-works';
 import { LandingTrustStripComponent } from '../../components/landing-trust-strip/landing-trust-strip';
 import { AuthService } from '@auth0/auth0-angular';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-landing-page',
@@ -25,8 +28,20 @@ import { AuthService } from '@auth0/auth0-angular';
 export class LandingPage {
   readonly currentYear = new Date().getFullYear();
 
-
-  constructor(private authService : AuthService){}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private destroyRef: DestroyRef
+  ) {
+    this.authService.isAuthenticated$
+      .pipe(
+        filter((isAuthenticated) => isAuthenticated),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(() => {
+        void this.router.navigate(['/home']);
+      });
+  }
 
   onLogin(): void {
     this.authService.loginWithRedirect();
