3650c16a835275f8439b7277413ec811b362d34b
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-04-22 08:32:02 +0200

    refactor(layout): Add different layouts for landing and app main components

--- src/app/features/landing/landing.routes.ts
+++ src/app/features/landing/landing.routes.ts
@@ -1,12 +1,19 @@
 import type { Routes } from '@angular/router';
+import { PublicLayout } from '../../layouts/public-layout/public-layout';
 
 export function provideLandingRoutes(): Routes {
   return [
     {
       path: '',
-      pathMatch: 'full',
-      loadComponent: () =>
-        import('./pages/landing-page/landing-page').then((m) => m.LandingPage),
+      component: PublicLayout,
+      children: [
+        {
+          path: '',
+          pathMatch: 'full',
+          loadComponent: () =>
+            import('./pages/landing-page/landing-page').then((m) => m.LandingPage),
+        },
+      ],
     },
   ];
 }
