171b20f980ef97c5b28bb1a98dc7ee06f47d7235
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-04-27 18:09:18 +0300

    refactor(environment): Update the api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://localhost:7254/',
   phase1: {
     feedPageSize: 20,
     maxFeedRetryCount: 3,
