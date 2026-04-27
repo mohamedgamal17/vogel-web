6096a82cd69bb2ad97dca8f3a2d225a45310101a
mohamedgamal17 <mohamedsherf17@gmail.com>
2026-04-27 19:58:41 +0300

    feat(auth): Add auth0 interceptors with httpclient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAuth0 } from '@auth0/auth0-angular';
+import { authHttpInterceptorFn, provideAuth0 } from '@auth0/auth0-angular';
 
 import { routes } from './app.routes';
 import { environment } from '../environments/environment';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideHttpClient(withInterceptors([authHttpInterceptorFn])),
     provideAuth0({
       domain: environment.auth0.domain,
       clientId: environment.auth0.clientId,
@@ -16,6 +18,18 @@ export const appConfig: ApplicationConfig = {
         redirect_uri: window.location.origin + '/',
         audience: environment.auth0.audience,
       },
-    })
-  ]
+      httpInterceptor: {
+        allowedList: [
+          {
+            uri: `${environment.apiUrl}/*`,
+            tokenOptions: {
+              authorizationParams: {
+                audience: environment.auth0.audience,
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
 };
